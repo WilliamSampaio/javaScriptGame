60b75d9f5c2c4ce6b7ec317b63aa3207732c3d71
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-07-07 00:59:33 -0400

    setup socket.io

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 import express from 'express'
-import http from 'http'
+import { createServer } from 'http'
+import createGame from './public/game.js'
+import { Server } from 'socket.io'
 
 const app = express()
-const server = http.createServer(app)
+const server = createServer(app)
+const socketio = new Server(server)
 
 app.use(express.static('public'))
 
-server.listen(3123, () => {
-    console.log('> server listening! on port 3123')
+const game = createGame()
+
+// game.addPlayer({ name: 'William', position: { x: 0, y: 0 } })
+// game.addFruit({ position: { x: 20, y: 9 } })
+// game.addFruit({ position: { x: 10, y: 5 } })
+
+console.log(game.players)
+
+socketio.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log('Server: Player conectado! ' + playerId)
+
+    socketio.emit('setup', { players: game.players, fruits: game.fruits, canvas: game.canvas })
+})
+
+server.listen(3000, () => {
+    console.log('> server listening! on port 3000')
 })
